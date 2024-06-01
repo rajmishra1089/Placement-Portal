89683b7aa18ee1ec07f181bba3d85aaa102f0356
rajmishra1089 <rajmishra1089@gmail.com>
2024-06-01 22:00:31 +0530

    Added index.js

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+ 
+app.use(express.json());
+
+app.use(cors({origin: 'http://localhost:3001',credentials: true })); 
+require('dotenv').config();
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+ 
+
+
+const PORT = process.env.PORT || 4000;
+
+//To connect to database
+const dbConnect = require('./config/database'); 
+dbConnect();  
+
+
+const user = require('./routes/user');
+const placement = require('./routes/placement')
+ 
+app.use('/vplace/v1/user',user)
+app.use('/vplace/v1/job',placement) 
+
+app.listen(PORT,()=>{
+    console.log(`App is running on ${PORT}`);
+})
+
+  
